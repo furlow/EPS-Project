650289beea5a20c9e0a533bc8b32cba5215d061c
Joshua Furlow <jf00109@surrey.ac.uk>
2013-12-10 01:17:54 +0000

    reconfigured bluetooth_pairing to thread class, still needs work

--- bluetooth_pairing.py
+++ bluetooth_pairing.py
@@ -5,6 +5,7 @@ import sys
 import dbus
 import dbus.service
 import dbus.mainloop.glib
+import threading
 
 class Agent(dbus.service.Object):
 	exit_on_release = True
@@ -14,30 +15,25 @@ class Agent(dbus.service.Object):
 		print "RequestPinCode (%s)" % (device)
 		return '0000'
 
-if __name__ == '__main__':
-	dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+class bluetooth_pairing(threading.Thread):
 	
-	bus = dbus.SystemBus()
-	
-	manager = dbus.Interface(bus.get_object("org.bluez", "/"), "org.bluez.Manager")
-	
-	capability = "KeyboardDisplay"
+	def __init__(self):
+		
+		threading.Thread.__init__(self)
+		self.capability = "KeyboardDisplay"
+		self.path = "/test/agent"
+		dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+		manager = dbus.Interface(dbus.SystemBus().get_object("org.bluez", "/"), "org.bluez.Manager")
+		path = manager.DefaultAdapter() #gets the path of the default bluetooth device
+		self.adapter = dbus.Interface(dbus.SystemBus().get_object("org.bluez", path), "org.bluez.Adapter")
+		
+	def run(self):
+		
+		agent = Agent(self.bus, self.path)
 
-	path = manager.DefaultAdapter() #gets the path of the default bluetooth device
+		mainloop = gobject.MainLoop()
 
-	adapter = dbus.Interface(bus.get_object("org.bluez", path), "org.bluez.Adapter")
-	
-	print adapter
+		self.adapter.RegisterAgent(self.path, self.capability)
+		print "Agent registered"
 
-	path = "/test/agent"
-	
-	agent = Agent(bus, path)
-
-	print agent
-
-	mainloop = gobject.MainLoop()
-
-	adapter.RegisterAgent(path, capability)
-	print "Agent registered"
-
-	mainloop.run()
+		mainloop.run()
