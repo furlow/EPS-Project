a9ec32fa070dc94852d117a0e82fce4b85fe554c
Joshua Furlow <englandrules1992@gmail.com>
2013-12-11 22:17:10 +0000

    Fix #8 and renamed variable
    renamed vairables so that they reflect what they are rather than being
    generic such as j or p

--- alarmmodules.py
+++ alarmmodules.py
@@ -9,7 +9,7 @@ class alarm():
                 GPIO.setmode(GPIO.BOARD)                                                                        # Set pin convention.
                 GPIO.setwarnings(False)
                 GPIO.setup(13, GPIO.OUT, initial= GPIO.LOW)                                     # Set up channel 13 to be output and set the output to low.
-                self.p = GPIO.PWM(13, 100)
+                self.light = GPIO.PWM(13, 100)
 
                 GPIO.setup(22, GPIO.IN, initial= GPIO.LOW)
                 
@@ -18,19 +18,19 @@ class alarm():
                 self.channelA = pygame.mixer.Channel(1)
         
         def lighton(self):                                                   # Channel=13, Frequency=100Hz.
-                self.p.start(100)                                                                                            # Set duty cycle to 100.
+                self.light.start(100)                                                                                            # Set duty cycle to 100.
                 print "Dutycycle set to 100"
                 return
 
         def lightoff(self):
-                self.p.stop()                                                                                                        # Stop PWM                                                                                         # Changes all channels used by script back to inputs.
+                self.light.stop()                                                                                                        # Stop PWM                                                                                         # Changes all channels used by script back to inputs.
                 print "Dutycycle set to 0"
                 return
 
         def alarmoff(self):
-                self.p.stop()                                                                                                        # Stop PWM                                                                                        # Changes all channels used by script back to inputs.
+                self.light.stop()
                 self.channelA.stop()
-                self.a=0
+                #self.a=0 #what does this line do?
                 print "Alarm has been turned off"
                 return
 
@@ -40,19 +40,19 @@ class alarm():
                 print "In Alarm ", list
                 self.channelA.set_volume(0.0)                                                                                                        #test
                 self.channelA.play(self.sound, -1)
-                j=1
+                volume=1
                 #turn on leds
 
-                self.p.start(0)                                                                                                      # Start PWM with duty cycle of 0.
+                self.light.start(0)                                                                                                      # Start PWM with duty cycle of 0.
 
                 print "Turning on the LEDs and Audio"
 
                 try:
                         for dutycycle in range(0, 101, 1):                                      # Use dutycycle of 0,5,10,15,20....100.         IS THIS ':' NEEDED?
                                 
-                                self.channelA.set_volume(j)
+                                self.channelA.set_volume(volume)
                                 self.p.ChangeDutyCycle(dutycycle)                                    # Change dutycycle.
-                                #j=j+0.01
+                                #volume=volume+0.01
                                 sleep(50)                                                                          # Delays the code for 60secs.
                                 self.a=1
                                 
@@ -65,7 +65,13 @@ class alarm():
                                                                                                         # time.sleep(1800)                                                                      # Then delay by 30mins/ stay on until turned off 
                                                         
                 except KeyboardInterrupt:
-                        raise
+                    close()
+                    raise
                         
                 return
+
+        def close(self):
+                self.channelA.stop()
+                pygame.mixer.quit()
+
 
