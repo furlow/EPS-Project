5ae0cf01042a6a2f4ee3dc52cca8cd0f0d9ef5f3
furlow <englandrules1992@gmail.com>
2013-12-11 23:13:01 +0000

    Add error handling and stop function
    -Added KeyboardInterrupt error handling to gracefully close
    resources on program quit
    -filled in the stop function
    -Added an intailasing of the client_sock class so that the
    stop function can be called on exit even if the server
    hasn't accept a connection yet

--- bluetooth_module.py
+++ bluetooth_module.py
@@ -7,49 +7,53 @@ class bluetooth_comms(threading.Thread):
         threading.Thread.__init__(self)
 
         self.data = data
-        self.keepalive = True
         
         port = 5
         backlog = 1
             
-       # self.server_sock=BluetoothSocket( RFCOMM )
-       # self.server_sock.bind(("",port))
-       # self.server_sock.listen(backlog)
+        self.server_sock=BluetoothSocket( RFCOMM )
+        self.server_sock.bind(("",port))
+        self.server_sock.listen(backlog)
 
-       # uuid = "df0677bc-5f0b-45e4-8207-122adee18805"
+	self.client_sock = BluetoothSocket( RFCOMM)
+
+        uuid = "df0677bc-5f0b-45e4-8207-122adee18805"
         
-       # advertise_service( self.server_sock, "alarm",
-       #                     service_id = uuid,
-       #                     service_classes = [ uuid, SERIAL_PORT_CLASS],
-       #                     profiles = [SERIAL_PORT_PROFILE])
+        advertise_service( self.server_sock, "alarm",
+                            service_id = uuid,
+                            service_classes = [ uuid, SERIAL_PORT_CLASS],
+                            profiles = [SERIAL_PORT_PROFILE])
 
     def run(self):
 
-        while(self.keepalive):
+	try:
+        
+		while(True):
 
-            print "waiting for connection..."
-            sleep(2)
-           # client_sock, client_info = self.server_sock.accept()
-            print "Accepted connection from "#, client_info
-            sleep(2)
-            print "waiting for data..."
-           # raw_data = client_sock.recv(1024)
+			print "waiting for connection..."
+            		sleep(2)
+            		self.client_sock, client_info = self.server_sock.accept()
+			
+			print "Accepted connection from "#, client_info
+            		sleep(2)
+            		print "waiting for data..."
+            		raw_data = self.client_sock.recv(1024)
 
-            raw_data = "1910;1912;1;0";
-            current_time = raw_data[0:4];
-            alarm_time = raw_data[5:9];
-            alarm = raw_data[10];
-            light = raw_data[12];
-            self.data[2] = 1 #all the above lines need to be changed to this format
-            #client_sock.send ("data sent")
-            #self.data = [int(current_time),int(alarm_time),int(alarm),int(light)]
-            print self.data
+            		raw_data = "1910;1912;1;0";
+            		current_time = raw_data[0:4];
+            		alarm_time = raw_data[5:9];
+            		alarm = raw_data[10];
+            		light = raw_data[12];
+            		self.data[2] = 1 #all the above lines need to be changed to this format
+            		self.client_sock.send ("data sent")
+            		#self.data = [int(current_time),int(alarm_time),int(alarm),int(light)]
+            		print self.data
 
-        #stop_advertising (self.server_sock)
-        #client_sock.close()
-        #self.server_sock.close()
+	except KeyboardInterrupt:
+		self.stop()
 
-    #def stop(self):
-    #    
-    #    self.keepalive = False
-
+    def stop(self):
+	self.keepalive = False
+        stop_advertising (self.server_sock)
+	self.client_sock.close()
+        self.server_sock.close()
