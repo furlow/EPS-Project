ae171cf838d03d58b573689c55fda5e66ea21457
Joshua Furlow <englandrules1992@gmail.com>
2013-12-12 15:41:55 +0000

    Update install.sh comments

--- install.sh
+++ install.sh
@@ -12,21 +12,24 @@ init_dir='/etc/init.d/'
 
 apt-get install bluez python-bluetooth gobject
 
-#copy the files to installation directories
+#Create installation directories
 echo "Creating installation directories"
 mkdir $install_dir
 
+#Copy the files to installation directories
 echo "Copying files"
 cp src/* $install_dir
 cp init/* $init_dir
 
+#Set file permissions of startup script & main program
 echo "Setting file permissions"
 chmod +x $init_dir/$LSB_Script
 chmod +x $install_dir/$main_prog
 
+#Add start up script to load on startup
 update-rc.d $LSB_Script defaults
 
 echo "Installation Complete"
-
 echo "Natures-alarm will auto start on reboot"
+
 echo "To Start now type 'sudo service natures-alarm start' in terminal"
