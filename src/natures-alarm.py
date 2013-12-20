d7034fac3b99c6a981b49710fc67ce99dc7a1f88
Sageeve Sudan <ss00354@surrey.ac.uk>
2013-12-20 00:24:18 +0000

    Added pin variables and fixed errors with button code
    - Added variables for the pins
    
    - Fixed error in the button code, was turning on and
      staying on, because the sub if statements where
      both ifs and not if, else. If the button was pressed
      it would turn the light value off and then back
      on again stright away.
    
    - Still need to increase stability with the button
      code and create a class to make things easier
    
    - Moved passing list to the init function of the
      alarmmodule

--- src/natures-alarm.py
+++ src/natures-alarm.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 
 import RPi.GPIO as GPIO 
 from alarmmodules import *
@@ -7,6 +7,10 @@ from time import sleep
 from threading import Timer
 from bluetooth_pairing_module import bluetooth_pairing
 
+LED_OUT_PIN = 13
+BUTTON_IN_PIN = 22
+
+
 
 try:
 
@@ -14,45 +18,56 @@ try:
 	
 	#Start the pairing thread
 	pairing = bluetooth_pairing()
-	pairing.daemon = True
-	pairing.start()
-	
+
 	#Start the bluetooth communications thread
         blue_comms = bluetooth_comms(list)
         blue_comms.daemon = True
-
-        print "In Main ", list      
-
         blue_comms.start()
 
         print "In Main ", list
 
-        alarm1 = alarm()
+        alarm1 = alarm(list, BUTTON_IN_PIN, LED_OUT_PIN)
 	totalsecs=0
+
 	print "Current totalsecs is 0"
+
         while 1:
-                
-                sleep(3)
+                sleep(0.1)
                 print "In Main ", list
 
-                if(list[2]== 1):
-                        totalsecs = alarm1.comparetime(list)
+		if(pairing.isAlive()):
+			print "Pairing Live"
+
+
+                if(list[2] == 1):
+                        totalsecs = alarm1.comparetime()
                         print "Totalsecs in main is", totalsecs
-                        t= Timer (int(totalsecs),alarm1.alarmon(list))
+                        t= Timer ( int(totalsecs), alarm1.alarmon())
                         print "Turning alarm on"
                         t.start()                                                      #function to turn on alarm
 
-                if(list[3]== 1):
+                if(list[3] == 1):
                         alarm1.lighton()
 
-                if(list[3]== 0):
+                if(list[3] == 0):
                         alarm1.lightoff()
-                        
-                if (GPIO.input(22)== 1):          #if button is pressed
-                        if (list[3]== 1):         #and light is on
-                                list[3]= 0        #then turn off light
-                        if (list[3]== 0):         #but if light is off
-                                list[3]= 1        #then turn on light
+                      
+		button_value = GPIO.input(BUTTON_IN_PIN)          #if button is pressed
+ 		print "Button Value is ", button_value
+		if(button_value == 1):
+			print "The Button was pressed"
+                        if(list[3] == 1):
+				print "Turning Light off"         #and light is on
+				pairing.stop()
+				if(not pairing.isAlive()):
+					print "Pairing stopped"
+                                list[3] = 0        #then turn off light
+                        else:
+				print "Turning Light on"         #but if light is off
+				pairing = bluetooth_pairing()
+				pairing.daemon = True
+				pairing.start()
+                                list[3] = 1        #then turn on light
                 
 except KeyboardInterrupt:
         blue_comms.stop()
