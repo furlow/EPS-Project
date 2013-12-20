372cc6dbc9a2efa521d4ee457308b4feca8c9999
Sageeve Sudan <ss00354@surrey.ac.uk>
2013-12-20 01:02:28 +0000

    Updated permissions

--- src/Utilities/soundon.py
+++ src/Utilities/soundon.py
@@ -5,7 +5,7 @@ pygame.mixer.init(22050,-16,1,4096)
 sound = pygame.mixer.Sound("Music/Play Hard.wav") 
 channelA = pygame.mixer.Channel(1) 
 channelA.set_volume(1)                                                                                                 
-channelA.play(sound, -1)
+channelA.play(sound)
 sleep(200)
 
 # self.channelA.stop()
