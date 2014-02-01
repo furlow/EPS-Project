320bbbd5b2af93256f842f4c7991fc142d383ce2
Joshua Furlow <englandrules1992@gmail.com>
2014-02-01 23:41:01 +0000

    #13 Updated with the app_data class that replaces the primitive list

--- src/natures-alarm.py
+++ src/natures-alarm.py
@@ -12,64 +12,60 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(BUTTON_IN_PIN, GPIO.IN)
 
 try:
-
-        list = [0,0,0,0]
-	#list = [513,513,1,0]
+    data = app_data()
 	
 	#Start the pairing thread
 	#pairing = bluetooth_pairing()
 
 	#Start the bluetooth communications thread
-        blue_comms = bluetooth_comms(list)
-        blue_comms.daemon = True
-        blue_comms.start()
-
-        print "In Main ", list
-
-        alarm1 = alarm(list)
-	totalsecs=0
+    blue_comms = bluetooth_comms(data)
+    blue_comms.daemon = True
+    blue_comms.start()
+    
+    main_alarm = alarm(data)
 
 	print "Current totalsecs is 0"
 
-        while 1:
-                sleep(0.1)
-                print "In Main ", list
+    while 1:
+        sleep(0.1)
+        
+        data.print_settings()
 
 		#if(pairing.isAlive()):
 		#	print "Pairing Live"
 
 
-                if(list[2] == 1):
-                        totalsecs = alarm1.comparetime()
-                        print "Totalsecs in main is", totalsecs
-                        t = Timer ( totalsecs, alarm1.alarmon )
-			t.daemon = True
-                        print "Turning alarm on"
-                        t.start()                                                      #function to turn on alarm
-			list[2] = 0
+        if(data.alarm_status() == True):
+            totalsecs = main_alarm.comparetime()
+            print "Totalsecs in main is", totalsecs
+            alarm_timer = Timer ( totalsecs, main_alarm.alarmon )
+			alarm_timer.daemon = True
+            print "Turning alarm on"
+            alarm_timer.start()
+			data.set_alarm_status(False)
 
-                #if(list[3] == 1):
-                #        alarm1.lighton()
+            #if(list[3] == 1):
+            #   alarm1.lighton()
 
-                #if(list[3] == 0):
-                #        alarm1.lightoff()
-                
-		button_value = GPIO.input(BUTTON_IN_PIN)          #if button is pressed
+            #if(list[3] == 0):
+            #   alarm1.lightoff()
+        
+        #Move this into and class
+		button_value = GPIO.input(BUTTON_IN_PIN)
  		print "Button Value is ", button_value
 
 		if(button_value == 1 and button_value_previous == 0):
-			print "The Button was pressed"
-                        if(list[3] == 0):
-				print "Turning Light on"
-				alarm1.lighton()
-                                list[3] = 1        #then turn off light
-                        else:
-				print "Turning Light off"         #but if light is off
+            print "The Button was pressed"
+            
+            if(data.light_status == False):
+                alarm1.lighton()
+                data.set_light_status(True)
+            else:
 				alarm1.lightoff()
-                                list[3] = 0     #then turn on light
+                data.set_light_status(False)
 		button_value_previous = button_value
                 
 except KeyboardInterrupt:
-        blue_comms.stop()
-        alarm1.close()
-        raise
+    blue_comms.stop()
+    alarm1.close()
+    raise
