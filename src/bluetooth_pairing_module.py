2f0308fc3ab3a8c1c8c9997f7641ca34637b33fd
Joshua Furlow <englandrules1992@gmail.com>
2014-02-01 21:02:08 +0000

    Made some changes to bluetooth pairing module

--- src/bluetooth_pairing_module.py
+++ src/bluetooth_pairing_module.py
@@ -6,63 +6,69 @@ import dbus
 import dbus.service
 import dbus.mainloop.glib
 import threading
+from time import sleep
+
+gobject.threads_init()
+
 
 # The Agent class inherits the dbus.service.Object class, it uses the default
 # initialiser of the parent class dbus.service object
 class Agent(dbus.service.Object):
-	exit_on_release = True
+    exit_on_release = True
     
     # Initalize the class and base class
     def __init__(self, bus, path):
-        dbus.service.Object.__init__(self, bus, path, loop)
-
+        dbus.service.Object.__init__(self, bus, path)
+    
     # This exports a dbus service method using the @dbus.service.method
     # decorator, which essential lets us define what happens
     # when this signal is called for the RequestPinCode function.
     @dbus.service.method("org.bluez.Agent", in_signature="o", out_signature="s")
-	def RequestPinCode(self, device):
-		print "RequestPinCode (%s)" % (device)
-		return '0000' #Set this to what ever value you want the pincode to be
-
+    def RequestPinCode(self, device):
+        print "RequestPinCode (%s)" % (device)
+        return '0000' #Set this to what ever value you want the pincode to be
 
 def create_device_reply(device):
-	print "New device (%s)" % (device)
-	loop.quit()
+    print "New device (%s)" % (device)
+    mainloop.quit()
 
 def create_device_error(error):
-	print "Creating device failed: %s" % (error)
+    print "Creating device failed: %s" % (error)
 
 
 #The Bluetooth pairing class inherits the blue threading. Thread class
 class bluetooth_pairing(threading.Thread):
+    
+    def __init__(self):
+        threading.Thread.__init__(self)
+    
+    #This is run by using the start() function for an instance of bluetooth_pairing
+    def run(self):
 
-	def __init__(self):
-		threading.Thread.__init__(self)
+        self.capability = "KeyboardDisplay"
+        self.path = "/test/agent"
 
-	#This is run by using the start() function for an instance of bluetooth_pairing
-	def run(self):
-        
-		self.capability = "KeyboardDisplay"
-		self.path = "/test/agent"
-        
-		dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-		self.bus = dbus.SystemBus()
-		manager = dbus.Interface(self.bus.get_object("org.bluez", "/"), "org.bluez.Manager")
-        
-		adapther_path = manager.DefaultAdapter() #gets the path of the default bluetooth device
-		self.adapter = dbus.Interface(self.bus.get_object("org.bluez", adapther_path), "org.bluez.Adapter")
-        
-        self.mainloop = gobject.MainLoop()
-		agent = Agent(self.bus, self.path, sel.mainloop)
-        
-		self.adapter.RegisterAgent(self.path,
-                                   self.capability,
-                                   reply_handler=create_device_reply,
-                                   error_handler=create_device_error)
-		print "Ready to pair"
-		self.mainloop.run()
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.bus = dbus.SystemBus()
+        manager = dbus.Interface(self.bus.get_object("org.bluez", "/"), "org.bluez.Manager")
+
+        adapther_path = manager.DefaultAdapter() #gets the path of the default bluetooth device
+        self.adapter = dbus.Interface(self.bus.get_object("org.bluez", adapther_path), "org.bluez.Adapter")
+
+        #mainloop = gobject.MainLoop()
+        agent = Agent(self.bus, self.path)
+
+        self.adapter.Discoverable = True
+        self.adapter.RegisterAgent(self.path,
+                           self.capability)
+                           
+        print "Ready to pair"
+            while True:
+                sleep(1)
 
 # Need to come up with a way in which the mainloop will quit, using the Thread quit method is okay.
 # However We can run the thread when the button is pressed down for 3 seconds. When a succesfull pair
 # is made, find out what org.bluez.agent method will be called when a succesfull pair has been created
 # we can then call quit() to quit the main loop and thus quit the thread.
+pair = bluetooth_pairing()
+pair.start()
